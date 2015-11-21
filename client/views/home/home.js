8d171f6428befc3d313adb1ecbdb2764b9d821df
Stefan Ilie <ilie.stefan92@yahoo.com>
2015-11-21 23:24:14 +0200

    ceva

--- client/views/home/home.js
+++
@@ -1,8 +0,0 @@
-if(Meteor.isCordova){
-  Meteor.startup(function(){
-    navigator.contacts.find(["*"], function(contacts){
-      alert('Found ' + contacts.length + ' contacts.');
-      console.log(contacts);
-    })
-  })
-}
