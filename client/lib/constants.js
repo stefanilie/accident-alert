b85836dc87069e7acbe55bc2e9a6a7dd8736b69e
Dragos <dragos@Ghitas-MacBook-Pro.local>
2015-11-21 20:44:32 +0200

    Routers

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -5,6 +5,6 @@ if (Meteor.App) {
 }
 
 Meteor.App = {
-  NAME: 'Meteor Boilerplate',
-  DESCRIPTION: 'A boilerplate for meteorjs projects http://matteodem.github.io/meteor-boilerplate/'
+  NAME: 'Accident Alert',
+  DESCRIPTION: 'Description something'
 };
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -6,3 +6,19 @@ Router.route('/', {
     SEO.set({ title: 'Home - ' + Meteor.App.NAME });
   }
 });
+
+Router.route('/settings', {
+  name: 'settings',
+  action: function () {
+    this.render('settings');
+    SEO.set({ title: 'Settings - ' + Meteor.App.NAME });
+  }
+});
+
+Router.route('/profile', {
+  name: 'profile',
+  action: function () {
+    this.render('profile');
+    SEO.set({ title: 'Profile - ' + Meteor.App.NAME });
+  }
+});
