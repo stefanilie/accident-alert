bd6d286b319bd013e95fc0656db02f8723c61d6f
Cristi <georgecristian.ivan@outlook.com>
2015-11-21 23:55:32 +0200

    Retrieve contacts method added.

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,8 +1,25 @@
-if(Meteor.isCordova){
-  Meteor.startup(function(){
-    navigator.contacts.find(["*"], function(contacts){
-      alert('Found ' + contacts.length + ' contacts.');
-      console.log(contacts);
-    })
-  })
-}
+Template.home.rendered = function () {
+    var emtyContactsList = [];
+    if(Meteor.isCordova){
+      function onSuccess(contacts){
+        Session.set("contacts",contacts);
+        console.log("Succes");
+      };
+      function onError(contactError){
+        Session.set("contacts","");
+        console.log("Failure");
+      };
+      var options = new ContactFindOptions();
+      options.multiple = true;
+      var fields       = ["displayName", "name"];
+      var contacts = navigator.contacts.find(fields, onSuccess, onError, options);
+    }else{
+      Session.set("contacts", emtyContactsList);
+    }
+};
+
+Template.home.helpers({
+  contacts: function () {
+    return Session.get("contacts");
+  } 
+});
