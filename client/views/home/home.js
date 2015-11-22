a4bc44a9cc7acd2c7470212697774f573e20294d
Dragos <dragos@Ghitas-MacBook-Pro.local>
2015-11-22 03:30:25 +0200

    Session monitor

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,8 +1,22 @@
-// Template.home.helpers({
-//   userPhoto: ''
-// });
+Template.home.onRendered(function(){
+  if (Session.get('monitor') === 'off' || Session.get('monitor') === undefined) {
+    $('.round-button-spinning').addClass('hidden');
+    $('.round-button').removeClass('hidden');
+    }
+  else {
+    $('.round-button').addClass('hidden');
+    $('.round-button-spinning').removeClass('hidden');
+  }
+});
 Template.home.events({
   'click .round-button': function() {
-    alert(1);
+    $('.round-button').addClass('hidden');
+    $('.round-button-spinning').removeClass('hidden');
+    Session.set('monitor','on');
+  },
+  'click .round-button-spinning': function() {
+    $('.round-button-spinning').addClass('hidden');
+    $('.round-button').removeClass('hidden');
+    Session.set('monitor','off');
   }
 });
