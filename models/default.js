f5a32af8c61a079691dc7771c35222ec2400d20e
Cristi <georgecristian.ivan@outlook.com>
2015-11-22 04:54:56 +0200

    People 2

--- models/default.js
+++ models/default.js
@@ -1,3 +1,11 @@
 // Use "orion generate model" to create new models
 // ...
 // Also creates files in server/publications
+
+People = new Meteor.Collection("people");
+
+People.allow({
+  insert: function (userId, doc) {
+    return true;
+  }
+});
